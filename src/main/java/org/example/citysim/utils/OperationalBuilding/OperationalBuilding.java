f468e3dbeebea3a7ab85ade9e138d8b89dd36816
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2024-11-16 18:36:20 +0000

    Initial vars no fields

---
+++ src/main/java/org/example/citysim/utils/OperationalBuilding/OperationalBuilding.java
@@ -0,0 +1,10 @@
+package org.example.citysim.utils.OperationalBuilding;
+
+import org.example.citysim.utils.Building;
+
+public abstract class OperationalBuilding extends Building {
+    protected int minWorkerCount;
+    protected int maxWorkerCount;
+    private int workerCount;
+
+}
