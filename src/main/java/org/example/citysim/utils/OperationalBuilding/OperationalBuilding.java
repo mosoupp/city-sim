10a4a79980e2853e1cca5e0aa453bd409f9dc745
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2024-11-17 13:58:34 +0000

    created Getters and Setters for vars

--- src/main/java/org/example/citysim/utils/OperationalBuilding/OperationalBuilding.java
+++ src/main/java/org/example/citysim/utils/OperationalBuilding/OperationalBuilding.java
@@ -8,5 +8,27 @@ public abstract class OperationalBuilding extends Building {
     protected int workerCount;
 
 
+    public int getMinWorkerCount() {
+        return minWorkerCount;
+    }
 
+    public void setMinWorkerCount(int minWorkerCount) {
+        this.minWorkerCount = minWorkerCount;
+    }
+
+    public int getMaxWorkerCount() {
+        return maxWorkerCount;
+    }
+
+    public void setMaxWorkerCount(int maxWorkerCount) {
+        this.maxWorkerCount = maxWorkerCount;
+    }
+
+    public int getWorkerCount() {
+        return workerCount;
+    }
+
+    public void setWorkerCount(int workerCount) {
+        this.workerCount = workerCount;
+    }
 }
