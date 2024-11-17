f81c13151250620912fb37eefebec8d78c5ab074
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2024-11-17 15:27:54 +0000

    Deleted test files

--- src/main/java/org/example/citysim/utils/OperationalBuilding/FoodBuilding/EthanTestMain1.java
+++
@@ -1,9 +0,0 @@
-package org.example.citysim.utils.OperationalBuilding.FoodBuilding;
-
-public class EthanTestMain1 {
-    public static void main(String[] args) {
-        Farm starterFarm = new Farm();
-        starterFarm.setFoodProdRate(1);
-        System.out.println(starterFarm.getFoodProdRate());
-    }
-}
