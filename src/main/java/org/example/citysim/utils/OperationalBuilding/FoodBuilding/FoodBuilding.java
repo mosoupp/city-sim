88763bb23d87eed01192bbea40d679564b40b333
Ethan Maya <92396506+EthanMaya@users.noreply.github.com>
2024-11-17 15:25:37 +0000

    Getters, Setters and Javadoc for FoodBuilding

--- src/main/java/org/example/citysim/utils/OperationalBuilding/FoodBuilding/FoodBuilding.java
+++ src/main/java/org/example/citysim/utils/OperationalBuilding/FoodBuilding/FoodBuilding.java
@@ -2,6 +2,30 @@ package org.example.citysim.utils.OperationalBuilding.FoodBuilding;
 
 import org.example.citysim.utils.OperationalBuilding.OperationalBuilding;
 
+/**
+ * Abstract class for Food buildings
+ * has a foodProdRate
+ */
+
 public abstract class FoodBuilding extends OperationalBuilding {
+    /**
+     * Rate at which food is produced
+     */
     protected int foodProdRate;
+
+    /**
+     * Get rate at which food is produced
+     * @return rate at which food is produced
+     */
+    public int getFoodProdRate() {
+        return foodProdRate;
+    }
+
+    /**
+     * Set rate at which food is produced
+     * @param foodProdRate the rate at which food is produced
+     */
+    public void setFoodProdRate(int foodProdRate) {
+        this.foodProdRate = foodProdRate;
+    }
 }
